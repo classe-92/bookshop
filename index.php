4dbd5daecf077b196ee87d5880e8c42a6ad031d3
klelia <clelia.fradella@gmail.com>
2023-05-15 09:27:44 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+include './partials/template/header.php';
+?>
+
+<h1>Books</h1>
+
+
+<?php
+include './partials/template/footer.php';
+?>
---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+
+include './partials/template/header.php';
+?>
+
+<h1>Arrivederci !</h1>
+
+<?php
+include './partials/template/footer.php';
+?>
