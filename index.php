f619ccb094d2d08a2046f4f6a25ce9d54d88ec75
klelia <clelia.fradella@gmail.com>
2023-05-15 10:21:48 +0200

    logout e redirect

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
-
+session_start();
+if (!isset($_SESSION['userId'])) {
+    //https: //www.php.net/manual/en/function.session-destroy.php
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(
+            session_name(),
+            '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+    session_destroy();
+    header("location: login.php");
+}
 include './partials/template/header.php';
 ?>
 
